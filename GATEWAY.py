8c7b0fc1d5d949264a61ee6f7e4d80e9938f51b8
kimhao97 <nguyenkimhaodn@gmail.com>
2019-11-06 10:03:41 +0000

    USB port

--- GATEWAY.py
+++ GATEWAY.py
@@ -1,1 +1,17 @@
+import serial 
+import time
+print("Begin....")
+usb_Serial = serial.Serial(             
+               port='/dev/ttyUSB3',
+               baudrate = 9600,
+               parity=serial.PARITY_NONE,
+               stopbits=serial.STOPBITS_ONE,
+               bytesize=serial.EIGHTBITS,
+               timeout=1
+    )
 
+while 1:
+
+	if usb_Serial.inWaiting()>0:
+		x=usb_Serial.readline()
+		print(x)
